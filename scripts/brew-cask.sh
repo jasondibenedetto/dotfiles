df553cf673efac2fdceb58eaf03f3c46c39eff66
jasondibenedetto <jason.dibenedetto6@gmail.com>
2017-12-07 20:24:52 +1030

    Stop using Brewfile

---
+++ scripts/brew-cask.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+brew cask install flux
+brew cask install font-hack
+brew cask install google-chrome
+brew cask install iterm2-beta
+brew cask install keepingyouawake
+brew cask install spectacle
---
+++ scripts/brew.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# install gnu formulas
+brew install coreutils
+brew install binutils
+brew install diffutils
+brew install ed --with-default-names
+brew install findutils --with-default-names
+brew install gawk
+brew install gnu-indent --with-default-names
+brew install gnu-sed --with-default-names
+brew install gnu-tar --with-default-names
+brew install gnu-which --with-default-names
+brew install gnutls
+brew install grep --with-default-names
+brew install gzip
+brew install screen
+brew install watch
+brew install wdiff --with-gettext
+brew install wget
+
+# upgrade existing gnu formulas
+brew install bash
+brew install emacs
+brew install gdb
+brew install gpatch
+brew install m4
+brew install make
+brew install nano
+
+# other formulas
+brew install direnv
+brew install fish
+brew install fisherman
+brew install fzf
+brew install git
+brew install less
+brew install neovim
+brew install node
+brew install openssh
+brew install python
+brew install rbenv
+brew install ripgrep
+brew install rsync
+brew install stow
+brew install vim --with-override-system-vi
+brew install yarn
+brew install z
