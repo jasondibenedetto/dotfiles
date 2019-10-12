35281c68b75d141940ea1479b71a247a1b223b1d
Jason Di Benedetto <jason.dibenedetto6@gmail.com>
2019-10-12 11:13:52 +1030

    dot

--- scripts/brew-cask.sh
+++ scripts/brew-cask.sh
@@ -3,8 +3,7 @@
 brew tap caskroom/versions
 brew tap caskroom/fonts
 
-brew cask install flux
 brew cask install font-source-code-pro
 brew cask install iterm2-beta
-brew cask install keepingyouawake
 brew cask install spectacle
+brew cask install visual-studio-code
