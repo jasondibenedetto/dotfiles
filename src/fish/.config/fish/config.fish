8d82b32ed0890e8ec32dfe7fc727ffdb0d8beb9f
Jason Di Benedetto <jason.dibenedetto6@gmail.com>
2020-01-25 13:30:56 +1030

    mango

--- src/fish/.config/fish/config.fish
+++ src/fish/.config/fish/config.fish
@@ -15,10 +15,3 @@ eval (direnv hook fish)
 
 # fzf
 set -x FZF_DEFAULT_COMMAND 'rg --files --hidden'
-
-# nix
-if test -e '/Users/jasondibenedetto/.nix-profile/etc/profile.d/nix.sh'
-  fenv source '/Users/jasondibenedetto/.nix-profile/etc/profile.d/nix.sh'
-end
-
-alias nix-fish 'nix-shell --command fish'
--- src/fish/.config/fish/paths.fish
+++ src/fish/.config/fish/paths.fish
@@ -1,10 +1,17 @@
 # brew
-set -g fish_user_paths $fish_user_paths '/usr/local/sbin'
+set PATH /usr/local/sbin $PATH
 
 # yarn
-set -g fish_user_paths $fish_user_paths "(yarn global bin)/node_modules/.bin"
+set PATH (yarn global bin)/node_modules/.bin $PATH
 
 # android studio
 set -x ANDROID_HOME $HOME/Library/Android/sdk
-set -g fish_user_paths $fish_user_paths $ANDROID_HOME/tools
-set -g fish_user_paths $fish_user_paths $ANDROID_HOME/platform-tools
+set PATH $ANDROID_HOME/tools $PATH
+set PATH $ANDROID_HOME/platform-tools $PATH
+set PATH $ANDROID_HOME/emulator $PATH
+set PATH $ANDROID_HOME/tools/bin $PATH
+
+set PATH /usr/local/opt/mongodb@3.4/bin $PATH
+
+set -g fish_user_paths "/usr/local/opt/php@7.2/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/php@7.2/sbin" $fish_user_paths
